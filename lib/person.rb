ad5835fbf2b2eaa43dc9879ecc93af7f96bdc93e
Leah Cardoz <leahcardoz@gmail.com>
2022-11-22 12:39:14 -0500

    commit tree adc72f9d99b853f44178c13d4fa918f3c62574b0

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,64 @@
-# your code goes here
+class Person
+
+    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def name
+        @name
+    end
+
+    def bank_account
+        @bank_account
+    end
+
+   def bank_account=(value)
+        @bank_account = value
+   end
+
+   def happiness
+        @happiness
+   end
+
+   def happiness=(value)
+        @happiness = value.clamp(0, 10)
+   end 
+
+   def hygiene
+        @hygiene
+   end
+
+   def hygiene=(value)
+        @hygiene = value.clamp(0, 10)
+   end
+
+   def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end 
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid (amount)
+        @bank_account = bank_account + amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        @hygiene = hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    
+end
